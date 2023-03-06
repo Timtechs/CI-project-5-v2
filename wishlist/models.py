1ced860c7f45df87274e78e33779d2d6c7689d7d
Timtechs <Timmy.s.deckert@gmail.com>
2023-03-06 06:05:13 +0000

    Added wishlist app

---
+++ wishlist/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    desired_product = models.ForeignKey(product, on_delete=models.CASCADE)
+    slug = models.CharField(max_length=254, null=True, blank=True)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    # update your model
+    def serialize(self):
+
+        return {
+            "id": self.desired_product.id,
+            "slug": self.slug
+        }
---
+++ wishlist/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from . import models
+
+
+def get_wishlists(request, *args, **kwargs):
+    qs = Wishlist.objects.all()
+    wishlist = [x.serialize() for x in qs]
+    data = {
+        "response": wishlist
+    }
+    return JsonResponse(data)
+
+
+def add_to_wishlist(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    desired_product, created = Wishlist.objects.get_or_create(desired_product=product, slug=product.slug, user=request.user,)
+    return redirect(get_wishlists)
+
+
+class WishListView (ListView):
+    model = Wishlist
+    template_name = 'templates/wishlist.html'
+    paginate_by = 10
+    context_object_name = 'wishlist'
+
+    def get_queryset(self):
+        return Wishlist.objects.filter(user=self.request.user)
+
+
+class ProductListView(LoginRequiredMixin, ListView):
+    model = Product
+    paginate_by = 10
+    template_name = 'products/products.html'
+    context_object_name = 'mygroups'
+
+    def get_queryset(self, **kwargs):
+        user = get_object_or_404(User, username=self.request.user.username)
+        return user.products.all()
